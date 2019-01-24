90aafa3d2e673a60e32d159a4c29ac3f88b5a628
Parv Mor <parvmor.pm@gmail.com>
2019-01-24 20:39:51 +0530

    Fix bug in run.sh
    Signed-off-by: Parv Mor <parvmor.pm@gmail.com>

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ mkdir output
 cd src
 for input in `ls ../tests/input1/`; do
     for config in `ls ../tests/cfg1/`; do
-        python2 lexer.py --cfg="../tests/cfg1/${config}" "../output/${input}_${config}.html" --out="../tests/input1/${input}"
+        python2 lexer.py --cfg="../tests/cfg1/${config}" --out="../output/${input}_${config}.html" "../tests/input1/${input}"
     done
 done
 cd ..
