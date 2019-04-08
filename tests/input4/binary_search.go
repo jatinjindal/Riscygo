78c8b7d7edcfb6f148c6a977173757d0ca71de5a
raghav garg <rohanji97@gmail.com>
2019-04-09 00:04:12 +0530

    implement /

---
+++ tests/input4/binary_search.go
@@ -0,0 +1,17 @@
+package main;
+import "fmt";
+
+func b_search(ar [10]int,key int,start int,end int){
+	var mid int
+}
+
+func main(){
+	var ar [10]int
+
+	for k:=0;k<10;k++{
+		ar[k]=k
+	}
+
+	printInt b_search(ar,7,0,9)
+
+}
